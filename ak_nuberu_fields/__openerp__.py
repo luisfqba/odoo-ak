dc3385619e24284d9d7df625a6cc253d42a4e733
luisfqba <luisfqba@gmail.com>
2015-08-14 03:37:43 -0500

    Actualizadas vistas de listas

--- ak_nuberu_fields/__openerp__.py
+++ ak_nuberu_fields/__openerp__.py
@@ -25,7 +25,7 @@
     'description': """Modulo que agrega nuevos campos a Modelos Nuberu""",
     'author': 'luisfqba@gmail.com',
     'update_xml': [
-        "mrp_view.xml","sale_view.xml","invoice_view.xml","stock_view.xml","purchase_view.xml","product_view.xml",
+        "mrp_view.xml","sale_view.xml","invoice_view.xml","stock_view.xml","purchase_view.xml","product_view.xml","account_voucher_view.xml",
         ],
     "depends": [
             "base","mrp","purchase","purchase_requisition",
