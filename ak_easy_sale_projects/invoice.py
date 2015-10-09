0b8fe191d24bae838fe7de9003141459dee3fba7
luisfqba <luisfqba@gmail.com>
2015-10-09 16:01:58 -0500

    Incremento de tamaño numero de factura

--- ak_easy_sale_projects/invoice.py
+++ ak_easy_sale_projects/invoice.py
@@ -6,5 +6,5 @@ class account_invoice(osv.Model):
     _inherit = 'account.invoice'
 
     _columns = {
-        'num_fact_proyecto': fields.char('Factura', size=10, help='Numero de factura del total de Proyecto'),
+        'num_fact_proyecto': fields.char('Factura', size=256, help='Numero de factura del total de Proyecto'),
     }
